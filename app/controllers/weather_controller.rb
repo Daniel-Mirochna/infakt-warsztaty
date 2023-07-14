f95508ea1771128ac3d8c18bf4a6619385904b20
Daniel Mirochna <DanielM6@protonmail.com>
2023-07-14 12:52:43 +0200

    Move longitude and latitude decalaration to geo_value private method

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,13 +2,15 @@ class WeatherController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 
   def show
-    latitude = weather_show_params[:latitude].to_f
-    longitude = weather_show_params[:longitude].to_f
-    @data = WeatherApiConnector.new.weather_data(latitude, longitude)
+    @data = WeatherApiConnector.new.weather_data(geo_value(:latitude), geo_value(:longitude))
     @weather_presenter = WeatherPresenter.new(@data)
   end
 
   private
+
+  def geo_value(name)
+    weather_show_params[name].to_f
+  end
 
   def weather_show_params
     params.permit(:latitude, :longitude)
