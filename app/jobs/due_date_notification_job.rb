42baa6e346cb2cb60dd5fc105f1fe584d41b1d1f
Daniel Mirochna <DanielM6@protonmail.com>
2023-07-14 14:18:53 +0200

    create private method books_loans

--- app/jobs/due_date_notification_job.rb
+++ app/jobs/due_date_notification_job.rb
@@ -2,7 +2,13 @@ class DueDateNotificationJob
 	include Sidekiq::Job
 
 	def perform
-		books_loan = BookLoan.where(status: 'checked_out', due_date: Date.tomorrow)
-		books_loan.each { |e| UserMailer.due_date_notification_email(e).deliver_now }
+		books_loans
+	end
+
+	private
+
+	def books_loans
+		loans = BookLoan.where(status: 'checked_out', due_date: Date.tomorrow)
+		loans.each { |book_loan| UserMailer.due_date_notification_email(book_loan).deliver_now }
 	end
 end
